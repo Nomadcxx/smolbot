c538707b68c7a25ba6975141959d2073a7ee394a
Nomadcxx <noovie@gmail.com>
2026-03-22 02:51:45 +1100

    feat(theme): add all theme configurations with full color palettes

--- internal/theme/themes/catppuccin.go
+++ internal/theme/themes/catppuccin.go
@@ -2,8 +2,8 @@ package themes
 
 func init() {
 	register("catppuccin", [15]string{
-		"#000000", "#000000", "#000000", "#585B70", "#89B4FA",
-		"#89B4FA", "#CBA6F7", "#F5C2E7", "#CDD6F4", "#A6ADC8",
-		"#F38BA8", "#F9E2AF", "#A6E3A1", "#89DCEB", "#585B70",
+		"#000000", "#000000", "#000000", "#313244", "#CBA6F7",
+		"#CBA6F7", "#89B4FA", "#A6E3A1", "#CDD6F4", "#A6ADC8",
+		"#F38BA8", "#F9E2AF", "#A6E3A1", "#89B4FA", "#313244",
 	})
 }
--- internal/theme/themes/tokyo_night.go
+++ internal/theme/themes/tokyo_night.go
@@ -1,9 +1,32 @@
 package themes
 
+import (
+	lipgloss "charm.land/lipgloss/v2"
+	"github.com/Nomadcxx/smolbot/internal/theme"
+)
+
 func init() {
 	register("tokyo_night", [15]string{
-		"#000000", "#000000", "#000000", "#414868", "#7AA2F7",
-		"#7AA2F7", "#BB9AF7", "#F7768E", "#C0CAF5", "#A9B1D6",
-		"#F7768E", "#E0AF68", "#9ECE6A", "#7DCFFF", "#414868",
+		"#000000", "#000000", "#000000", "#24283B", "#7AA2F7",
+		"#7AA2F7", "#BB9AF7", "#F7768E", "#C0CAF5", "#565F89",
+		"#F7768E", "#E0AF68", "#9ECE6A", "#7DCFFF", "#24283B",
+	}, func(t *theme.Theme) {
+		t.ToolName = lipgloss.Color("#7AA2F7")
+		t.TranscriptUserAccent = lipgloss.Color("#7AA2F7")
+		t.TranscriptAssistantAccent = lipgloss.Color("#BB9AF7")
+		t.TranscriptThinking = lipgloss.Color("#565F89")
+		t.TranscriptStreaming = lipgloss.Color("#7DCFFF")
+		t.TranscriptError = lipgloss.Color("#F7768E")
+		t.MarkdownHeading = lipgloss.Color("#7AA2F7")
+		t.MarkdownLink = lipgloss.Color("#7DCFFF")
+		t.MarkdownCode = lipgloss.Color("#BB9AF7")
+		t.SyntaxKeyword = lipgloss.Color("#7AA2F7")
+		t.SyntaxString = lipgloss.Color("#9ECE6A")
+		t.SyntaxComment = lipgloss.Color("#565F89")
+		t.ToolStateRunning = lipgloss.Color("#E0AF68")
+		t.ToolStateDone = lipgloss.Color("#9ECE6A")
+		t.ToolStateError = lipgloss.Color("#F7768E")
+		t.ToolArtifactHeader = lipgloss.Color("#221C2C")
+		t.ToolArtifactBody = lipgloss.Color("#141620")
 	})
 }
